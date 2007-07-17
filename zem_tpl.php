84522f2928340baf4923db1866982d469eb0504f
manfre <manfre@localhost>
2007-07-17 03:59:33 +0000

    Fixed require path.

--- zem_tpl.php
+++ zem_tpl.php
@@ -2,6 +2,6 @@
 
 // This is simply a redirect that allows me to keep multiple template files
 // and textile class files.
-include_once('./01_plugin-template/zem_tpl.php');
+require_once('4.0-plugin-template/zem_tpl.php');
 
 ?>
