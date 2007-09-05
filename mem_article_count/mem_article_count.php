6b7e26d4a4e8046a819c941e71807c3d571c38eb
manfre <manfre@localhost>
2007-09-05 00:40:27 +0000

    Added attribute 'status', which defaults to 4 to match legacy behavior. Tested with txp 4.0.5

--- mem_article_count/mem_article_count.php
+++ mem_article_count/mem_article_count.php
@@ -1,106 +1,75 @@
 <?php
 
-// Either copy classTextile.php to your plugin directory, or uncomment the following
-// line and edit it to give the location where classTextile.php can be found
-#ini_set('include_path', ini_get('include_path') . ':/full/path/to/textile');
+// This is a PLUGIN TEMPLATE.
 
-// Plugin name is optional.  If unset, it will be extracted from the current file name.
-// Uncomment and edit this line to override:
-#$plugin['name'] = 'mem_article_count';
+// Copy this file to a new name like abc_myplugin.php.  Edit the code, then
+// run this file at the command line to produce a plugin for distribution:
+// $ php abc_myplugin.php > abc_myplugin-0.1.txt
 
-$plugin['version'] = '0.1';
+// Plugin name is optional.  If unset, it will be extracted from the current
+// file name. Uncomment and edit this line to override:
+# $plugin['name'] = 'abc_plugin';
+
+// 0 = Plugin help is in Textile format, no raw HTML allowed (default).
+// 1 = Plugin help is in raw HTML.  Not recommended.
+# $plugin['allow_html_help'] = 1;
+
+$plugin['version'] = '0.2';
 $plugin['author'] = 'Michael Manfre';
 $plugin['author_uri'] = 'http://manfre.net/';
 $plugin['description'] = 'Returns the article count for the number of articles in a specific section and/or a category.';
 
-compile_plugin();
-exit;
+// Plugin types:
+// 0 = regular plugin; loaded on the public web side only
+// 1 = admin plugin; loaded on both the public and admin side
+// 2 = library; loaded only when include_plugin() or require_plugin() is called
+$plugin['type'] = 1; 
 
+if (!defined('txpinterface'))
+	@include_once('../zem_tpl.php');
+
+if (0) {
 ?>
 # --- BEGIN PLUGIN HELP ---
 
-<code>
-<pre>
+h1. Summary
+
+p. This plugin will return the count of all 
+
 Example using all possible attributes:
-<txp:mem_article_count section="textpattern" category="plugin" wraptag="div" />
-</pre>
-</code>
+@<txp:mem_article_count section="textpattern" category="plugin" wraptag="div" />@
 
 # --- END PLUGIN HELP ---
 <?php
-
+}
 
 # --- BEGIN PLUGIN CODE ---
 
+// Plugin code goes here.  No need to escape quotes.
 
 function mem_article_count ($atts)
 {
     extract(lAtts(array(
         'wraptag'   => '',
         'section'   => '',
-        'category'  => ''
+        'category'  => '',
+        'status'	=> '4'
     ),$atts));
 
-    $where = "status=4 and Posted < now()";
+    $where = array();
     
-    if ($section) $where .= " and section = '".$section."'";
-    if ($category) $where .= " and (Category1 = '".$category."' or Category2 = '".$category."')";
+    $where[] = 'posted <= now()';
+    
+    if (!empty($status) || $status==='0') $where[] = "status = ". assert_int($status);
+    if ($section) $where[] = "section = '".doSlash($section)."'";
+    if ($category) $where[] = "(Category1 = '".doSlash($category)."' or Category2 = '".doSlash($category)."')";
 
-    $total = safe_field('count(*)','textpattern',$where);
+    $total = safe_field('count(*)','textpattern', join(' and ', $where));
 
     return ($wraptag) ? tag($total,$wraptag) : $total;
 }
 
 
-
 # --- END PLUGIN CODE ---
-
-
-// -----------------------------------------------------
-
-function extract_section($lines, $section) {
-	$result = "";
-	
-	$start_delim = "# --- BEGIN PLUGIN $section ---";
-	$end_delim = "# --- END PLUGIN $section ---";
-
-	$start = array_search($start_delim, $lines) + 1;
-	$end = array_search($end_delim, $lines);
-
-	$content = array_slice($lines, $start, $end-$start);
-
-	return join("\n", $content);
-
-}
-
-function compile_plugin() {
-	global $plugin;
-
-	if (!isset($plugin['name'])) {
-		$plugin['name'] = basename(__FILE__, '.php');
-	}
-
-	# Read the contents of this file, and strip line ends
-	$content = file(__FILE__);
-	for ($i=0; $i < count($content); $i++) {
-		$content[$i] = rtrim($content[$i]);
-	}
-
-	$plugin['help'] = extract_section($content, 'HELP');
-	$plugin['code'] = extract_section($content, 'CODE');
-
-	@include('classTextile.php');
-	if (class_exists('Textile')) {
-		$textile = new Textile();
-		$plugin['help'] = $textile->TextileThis($plugin['help']);
-	}
-
-	$plugin['md5'] = md5( $plugin['code'] );
-
-	// to produce a copy of the plugin for distribution, load this file in a browser. 
-
-echo chr(60)."?php\n\n"."/*\n * Plugin: ".$plugin['name']." v".$plugin['version']."\n * Author: ".$plugin['author']."\n * Generated: ".date("m.d.Y G:i")."\n */\n\n".'$'."plugin='" . base64_encode(serialize($plugin)) . "'\n?".chr(62);
-
-}
 
 ?>
