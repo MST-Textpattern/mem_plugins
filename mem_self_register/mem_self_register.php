14ddbeff3b6507c1e7108550077a468f3e577ede
Manfre <Manfre@localhost>
2011-09-08 20:43:42 -0400

    mem_self_register - updated plugin template and added auto-enable ability.

--- mem_self_register/mem_self_register.php
+++ mem_self_register/mem_self_register.php
@@ -1,20 +1,66 @@
 <?php
 
+// This is a PLUGIN TEMPLATE.
+
 // Copy this file to a new name like abc_myplugin.php.  Edit the code, then
 // run this file at the command line to produce a plugin for distribution:
 // $ php abc_myplugin.php > abc_myplugin-0.1.txt
 
 // Plugin name is optional.  If unset, it will be extracted from the current
-// file name. Uncomment and edit this line to override:
+// file name. Plugin names should start with a three letter prefix which is
+// unique and reserved for each plugin author ('abc' is just an example).
+// Uncomment and edit this line to override:
 $plugin['name'] = 'mem_self_register';
+
+// Allow raw HTML help, as opposed to Textile.
+// 0 = Plugin help is in Textile format, no raw HTML allowed (default).
+// 1 = Plugin help is in raw HTML.  Not recommended.
+# $plugin['allow_html_help'] = 0;
 
 $plugin['version'] = '0.9.9';
 $plugin['author'] = 'Michael Manfre';
 $plugin['author_uri'] = 'http://manfre.net/';
 $plugin['description'] = 'User self registration. Read the help to install.';
-$plugin['type'] = 1; // 0 for regular plugin; 1 if it includes admin-side code
 
-@include_once('../zem_tpl.php');
+// Plugin load order:
+// The default value of 5 would fit most plugins, while for instance comment
+// spam evaluators or URL redirectors would probably want to run earlier
+// (1...4) to prepare the environment for everything else that follows.
+// Values 6...9 should be considered for plugins which would work late.
+// This order is user-overrideable.
+# $plugin['order'] = 5;
+
+// Plugin 'type' defines where the plugin is loaded
+// 0 = public       : only on the public side of the website (default)
+// 1 = public+admin : on both the public and admin side
+// 2 = library      : only when include_plugin() or require_plugin() is called
+// 3 = admin        : only on the admin side
+# $plugin['type'] = 1;
+
+// Plugin 'flags' signal the presence of optional capabilities to the core plugin loader.
+// Use an appropriately OR-ed combination of these flags.
+// The four high-order bits 0xf000 are available for this plugin's private use.
+if (!defined('PLUGIN_HAS_PREFS')) define('PLUGIN_HAS_PREFS', 0x0001); // This plugin wants to receive "plugin_prefs.{$plugin['name']}" events
+if (!defined('PLUGIN_LIFECYCLE_NOTIFY')) define('PLUGIN_LIFECYCLE_NOTIFY', 0x0002); // This plugin wants to receive "plugin_lifecycle.{$plugin['name']}" events
+
+$plugin['flags'] = PLUGIN_LIFECYCLE_NOTIFY;
+
+// Plugin 'textpack' is optional. It provides i18n strings to be used in conjunction with gTxt().
+// Syntax:
+// ## aritrary comment
+// #@event
+// #@language ISO-LANGUAGE-CODE
+// abc_string_name => Localized String
+
+$plugin['textpack'] = <<< EOT
+#@common
+#@language en-us
+
+EOT;
+
+
+if (!defined('txpinterface'))
+	@include_once('../zem_tpl.php');
 
 if (0) {
 ?>
@@ -237,6 +283,25 @@ p(tag-summary). Returns the number of users.
 ////////////////////////////////////////////////////////////
 require_plugin('mem_form');
 include_once txpath.'/lib/PasswordHash.php';
+
+if (@txpinterface == 'admin')
+{
+	register_callback('mem_self_auto_enable', 'plugin_lifecycle.mem_self_register', 'installed');
+}
+
+/** Automatically install mem_wine* plugins when installed */
+function mem_self_auto_enable($event, $step)
+{
+	$plugin = substr($event, strlen('plugin_lifecycle.'));
+
+	$prefix = 'mem_self_register';
+	if (strncmp($plugin, $prefix, strlen($prefix)) == 0)
+	{
+		safe_update('txp_plugin', "status = 1", "name = '" . doSlash($plugin) . "'");
+	}
+}
+
+
 
 // MLP
 global $mem_self_lang;
